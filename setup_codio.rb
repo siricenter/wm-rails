8c13c980370155fcd43ee0fc00c1f696beb4cbaf
CJPoll <cjpoll@gmail.com>
2014-07-17 22:34:02 +0000

    Scripts for setting up a codio environment

---
+++ setup_codio.rb
@@ -0,0 +1,76 @@
+#!/usr/bin/env ruby
+def bundle_install
+	puts `cd ~/workspace`
+	puts `bundle install`
+end
+
+def install_sqlite
+	unless `parts list`.match(/sqlite/)
+		puts `parts install sqlite3`
+	end
+end
+
+def install_mysql
+	unless `parts list`.match(/mariadb/)
+		puts `parts install mariadb`
+	end
+
+	puts `parts start mariadb`
+end
+
+def install_mongo
+	unless `parts list`.match(/mongodb/)
+		puts `parts install mongodb`
+	end
+
+	puts `parts start mongodb`
+end
+
+def setup_database
+	puts `mysql -u root < setup_database.sh`
+end
+
+def migrate
+	puts `rake db:migrate`
+	puts `rake db:seed`
+end
+
+def start_server
+	puts `cd ~/workspace`
+	puts `rails server -d`
+end
+
+def get_vimrc
+	unless File.exists? "/home/codio/.vimrc"
+		`cd ~`
+		puts `git clone https://github.com/CJPoll/devenvset`
+		puts `mv devenvset/files/default/.vimrc ~`
+		puts `rm -rf devenvset`
+		`cd ~/workspace`
+	end
+end
+
+def install_vundle
+	puts `git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim` unless File.directory? "/home/codio/.vim/bundle/"
+end
+
+def install_packages
+	packages = [
+		"ack",
+		"tmux"
+	]
+
+	packages.each do |package|
+		`parts install #{package}`
+	end
+end
+
+#install_mysql
+#install_mongo
+install_sqlite
+bundle_install
+setup_database
+start_server
+get_vimrc
+install_vundle
+install_packages
