b38f436996f27ba8ecbf5d5c0ad4e982efc08822
Cody Poll <CJPoll@gmail.com>
2014-07-22 04:50:02 -0600

    Reorganized stylesheets to be much cleaner

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,11 +1,13 @@
 class StaticController < ApplicationController
-  def landing
-	  @semesters = Semester.all
-  end
+	layout 'landing', except: [:choose]
 
-  def choose
-	  buildings = Building.all
-	  @mens = buildings.first
-	  @womens = buildings.last
-  end
+	def landing
+		@semesters = Semester.all
+	end
+
+	def choose
+		buildings = Building.all
+		@mens = buildings.first
+		@womens = buildings.last
+	end
 end
