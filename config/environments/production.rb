7d78ba767571ad73650fedf0f59e3bdd09eb789e
Cody Poll <CJPoll@gmail.com>
2014-08-08 13:57:25 -0600

    Changed credentials for production to be sendable from heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,8 +83,8 @@ Rails.application.configure do
 		:address   => "smtp.mandrillapp.com",
 		:port      => 587, # ports 587 and 2525 are also supported with STARTTLS
 		:enable_starttls_auto => true, # detects and uses STARTTLS
-		:user_name => "managers@windsormanor.net",
-		:password  => "0Qr7YFg0xDJYpkNlV3apTQ", # SMTP password is any valid API key
+		:user_name => "app27911098@heroku.com",
+		:password  => "Cj9ezxan1NMa9ik9UdV00Q", # SMTP password is any valid API key
 		:authentication => 'login', # Mandrill supports 'plain' or 'login'
 		:domain => 'windsormanor.net', # your domain to identify your server when connecting
 	}
