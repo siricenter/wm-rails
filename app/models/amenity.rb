d2356b85abf626b0ce846e01171d3ab448836064
ChadCarey <chadcarey7@gmail.com>
2014-09-02 19:30:14 +0000

    added hooks before and after destroy in the amenity model to remove it from cloudinary

--- app/models/amenity.rb
+++ app/models/amenity.rb
@@ -14,4 +14,19 @@ class Amenity < ActiveRecord::Base
 	validates :image_id, presence: true
 	validates :title, presence: true
 	validates :description, presence: true
+  
+  before_destroy :backupItem
+  after_destroy :deleteItem
+  
+  # saves the item id before destroying
+  def backupItem
+    @public_id = self.image_id.split('/').last
+    @public_id = @public_id.split('#').first
+    @public_id = @public_id.split('.').first
+  end
+  
+  # after destroying the image from the db this will remove it from cloudinary
+  def deleteItem
+    Cloudinary::Uploader.destroy(@public_id)
+  end
 end
