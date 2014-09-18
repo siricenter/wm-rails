4601fe4528c046b652daa658a066a66bdd3b5ae4
Cody Poll <CJPoll@gmail.com>
2014-09-18 08:32:30 -0600

    Fixed indentation of apartment model

--- app/models/apartment.rb
+++ app/models/apartment.rb
@@ -1,24 +1,24 @@
 class Apartment < ActiveRecord::Base
-  validates :image_id, presence: true
+	validates :image_id, presence: true
 	validates :title, presence: true
 	validates :description, presence: true
-  
-  before_destroy :backupItem
-  after_destroy :deleteItem
-  
-  # saves the item id before destroying
-  def backupItem
-    if self.image_id
-    	@public_id = self.image_id.split('/').last
-    	@public_id = @public_id.split('#').first
-    	@public_id = @public_id.split('.').first
-    end
-  end
-  
-  # after destroying the image from the db this will remove it from cloudinary
-  def deleteItem
-    if @public_id
-    	Cloudinary::Uploader.destroy(@public_id)
-    end
-  end
+
+	before_destroy :backupItem
+	after_destroy :deleteItem
+
+	# saves the item id before destroying
+	def backupItem
+		if self.image_id
+			@public_id = self.image_id.split('/').last
+			@public_id = @public_id.split('#').first
+			@public_id = @public_id.split('.').first
+		end
+	end
+
+	# after destroying the image from the db this will remove it from cloudinary
+	def deleteItem
+		if @public_id
+			Cloudinary::Uploader.destroy(@public_id)
+		end
+	end
 end
