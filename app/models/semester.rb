ad8661432bcec1b038e568989f2fafab3f9ad6be
Cody Poll <CJPoll@gmail.com>
2014-09-19 23:20:56 -0600

    Fixed stupid indentation in semester model

--- app/models/semester.rb
+++ app/models/semester.rb
@@ -17,12 +17,12 @@ class Semester < ActiveRecord::Base
 	has_and_belongs_to_many :contracts, dependent: :nullify
 
 	validates :name, presence: true, uniqueness: true
-    validates :deposit, presence: true, numericality: true
+	validates :deposit, presence: true, numericality: true
 	validates :rent, presence: true, numericality: true
 	validates :start_date, presence: true
 	validates :end_date, presence: true
 
 	def payment_due
-    self.start_date - 21 # three weeks
+		self.start_date - 21 # three weeks
 	end
 end
