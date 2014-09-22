6d25e455216a285ad3a8a810f03eea4de334fdba
Cody Poll <CJPoll@gmail.com>
2014-09-22 10:41:11 -0600

    Wrote out rspec to test that ajax#beds returns assigns a building's apartments

--- spec/controllers/ajax_controller_spec.rb
+++ spec/controllers/ajax_controller_spec.rb
@@ -2,6 +2,14 @@ require 'rails_helper'
 
 RSpec.describe AjaxController, :type => :controller do
 	describe 'beds' do
-		it 'Should return a list of apartments for a building'
+		it 'Should return a list of apartments for a building' do
+			apartment1 = FactoryGirl.create(:apartment)
+			apartment2 = FactoryGirl.create(:apartment, building: apartment1.building)
+			FactoryGirl.create(:apartment)
+			semester = FactoryGirl.create(:semester)
+
+			get :beds, {building_id: apartment1.building.to_param, semester_id: semester.to_param}
+			expect(assigns(:apartments)).to match_array([apartment1, apartment2])
+		end
 	end
 end
